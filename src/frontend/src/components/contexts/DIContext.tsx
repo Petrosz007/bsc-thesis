fb2ada2e19a94b3d0a4c8450cf69ea2fb683cc25
Petrosz007 <andipeti2000@gmail.com>
2021-04-02 11:42:20 +0200

    Refactored api url to config.ts

--- src/frontend/src/components/contexts/DIContext.tsx
+++ src/frontend/src/components/contexts/DIContext.tsx
@@ -4,8 +4,10 @@ import { AccountRepository, IAccountRepository } from "../../repositories/accoun
 import { AppointmentRepository, IAppointmentRepository } from "../../repositories/appointmentRepository";
 import { CategoryRepository, ICategoryRepository } from "../../repositories/categoryRepository";
 import { IUserRepository, UserRepository } from "../../repositories/userRepository";
+import {Config} from "../../config";
 
 export interface DIContextState {
+    config: Config;
     userRepo: IUserRepository;
     appointmentRepo: IAppointmentRepository;
     categoryRepo: ICategoryRepository;
@@ -13,17 +15,11 @@ export interface DIContextState {
 }
 
 class InitialState implements DIContextState {
-    constructor() {
-        this.userRepo = new UserRepository();
-        this.categoryRepo = new CategoryRepository(this.userRepo);
-        this.appointmentRepo = new AppointmentRepository(this.userRepo, this.categoryRepo);
-        this.accountRepo = new AccountRepository();
-    }
-
-    userRepo: IUserRepository;
-    appointmentRepo: IAppointmentRepository;
-    categoryRepo: ICategoryRepository;
-    accountRepo: IAccountRepository;
+    config: Config = new Config();
+    userRepo: IUserRepository = new UserRepository(this.config);
+    accountRepo: IAccountRepository = new AccountRepository(this.config);
+    categoryRepo: ICategoryRepository = new CategoryRepository(this.config, this.userRepo);
+    appointmentRepo: IAppointmentRepository = new AppointmentRepository(this.config, this.userRepo, this.categoryRepo);
 }
 
 export const DIContext = createContext<DIContextState>(new InitialState());
--- src/frontend/src/repositories/userRepository.ts
+++ src/frontend/src/repositories/userRepository.ts
@@ -2,6 +2,7 @@ import { IsLoggedInDTO } from "../logic/dtos";
 import { User } from "../logic/entities";
 import { ResultPromise } from "../utilities/result";
 import { safeApiFetchAs } from "./utilities";
+import {Config} from "../config";
 
 export interface IUserRepository {
     getByUserName(userName: string): ResultPromise<User,Error>;
@@ -9,11 +10,15 @@ export interface IUserRepository {
 };
 
 export class UserRepository implements IUserRepository {
+    constructor(
+        private readonly config: Config
+    ) {}
+    
     getByUserName = (userName: string): ResultPromise<User,Error> =>
-        safeApiFetchAs<User>(`https://localhost:44347/User/Info/${userName}`, 'GET');
+        safeApiFetchAs<User>(`${this.config.apiUrl}/User/Info/${userName}`, 'GET');
 
     getSelf = (): ResultPromise<string,Error|'NotLoggedIn'> =>
-        safeApiFetchAs<IsLoggedInDTO>('https://localhost:44347/User/Self', 'GET')
+        safeApiFetchAs<IsLoggedInDTO>(`${this.config.apiUrl}/User/Self`, 'GET')
             .andThen(res => res.userName === null 
                     ? ResultPromise.err('NotLoggedIn') 
                     : ResultPromise.ok(res.userName));
