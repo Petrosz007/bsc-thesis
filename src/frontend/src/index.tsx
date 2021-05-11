7a96e8de10b4ebafb6e55c6b31cc4f72f4e76828
Petrosz007 <andipeti2000@gmail.com>
2021-05-11 16:38:08 +0200

    Added 404 not found page

--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -25,6 +25,7 @@ import Report from './routes/Reports';
 import Reports from './routes/Reports';
 import NotificationProvider, { NotificationContext } from './components/contexts/NotificationProvider';
 import Profile from "./routes/Profile";
+import NotFound from "./routes/NotFound";
 
 const App = () => {
     return (
@@ -59,13 +60,14 @@ const Main = () => {
                 <div className="main-content">
                     <Switch>
                         <Route path="/contractor">          <Contractor />          </Route>
-                        <Route path ="/booked">             <Booked />              </Route>
-                        <Route path ="/own-appointments">   <OwnAppointments />     </Route>
-                        <Route path ="/reports">            <Reports />             </Route>
-                        <Route path ="/profile">            <Profile />             </Route>
-                        <Route path ="/login">              <Login />               </Route>
-                        <Route path ="/register">           <Register />            </Route>
-                        <Route path="/">                    <Home />                </Route>
+                        <Route path="/booked">              <Booked />              </Route>
+                        <Route path="/own-appointments">    <OwnAppointments />     </Route>
+                        <Route path="/reports">             <Reports />             </Route>
+                        <Route path="/profile">             <Profile />             </Route>
+                        <Route path="/login">               <Login />               </Route>
+                        <Route path="/register">            <Register />            </Route>
+                        <Route exact path="/">              <Home />                </Route>
+                        <Route>                             <NotFound />            </Route>
                     </Switch>
                 </div>
             </div>
